0c9f70530e22f3a44f755cb4feb5ea606f419d6b
Federico Wang <fwang@cpp.edu>
2017-07-28 20:12:15 -0700

    hi

---
+++ app/src/main/java/com/example/feder/myapplication/model/Account.java
@@ -0,0 +1,13 @@
+package com.example.feder.myapplication.model;
+
+/**
+ * Created by Feder on 7/28/2017.
+ */
+
+public class Account
+{
+    public int accountId;
+    public String accountName;
+
+
+}
---
+++ app/src/main/java/com/example/feder/myapplication/model/AccountTransaction.java
@@ -0,0 +1,14 @@
+package com.example.feder.myapplication.model;
+
+/**
+ * Created by Feder on 7/28/2017.
+ */
+
+public class AccountTransaction
+{
+    public int accountTransactionId;
+    public int amount;
+    public int accountId;
+    public String date;
+
+}
